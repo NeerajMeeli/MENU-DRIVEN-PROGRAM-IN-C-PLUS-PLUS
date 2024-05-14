5c334b058da63f6c1e8e49d55b6797241879b48d
Neeraj Kumar Thakur <neerajkumarthakur.dhaka@gmail.com>
2024-05-15 01:01:08 +0530

    Update Selection sort

--- All-DS-Sort-program-onesort.cpp
+++ All-DS-Sort-program-onesort.cpp
@@ -8,7 +8,7 @@ using namespace std;
         cout<<"Enter the size of an array:";
         cin>>n;
         int arr[n];
-        cout<<"Enter the value of array in jumbled order :"<<endl;
+        cout<<"Enter the value Ascending or jumbled order :"<<endl;
         for(int i=0;i<n;i++)
         {
             cin>>arr[i];
@@ -18,7 +18,7 @@ using namespace std;
         {
             for(int j=0;j<n-i-1;j++)
             {
-                if(arr[j]>arr[j+1])
+                if(arr[j]<arr[j+1])
                 {
                     int temp;
                     temp=arr[j];
@@ -27,7 +27,7 @@ using namespace std;
                 }
             }
         }
-        cout<<"Ascending order array element are:"<<endl;
+        cout<<"Discending order array element are:"<<endl;
         for(int i=0;i<n;i++)
         {
             cout<<arr[i]<<endl;
@@ -43,7 +43,7 @@ using namespace std;
         cout<<"Enter the size of an array:";
         cin>>n;
         int arr[n];
-        cout<<"Enter the value of an array:"<<endl;
+        cout<<"Enter the value of Only Jumble order:"<<endl;
         for(int i=0;i<n;i++)
         {
             cin>>arr[i];
@@ -61,7 +61,7 @@ using namespace std;
             arr[j+1]=key;
         }
 
-        cout<<"Print the Sorted array is :"<<endl;
+        cout<<"The all element Ascendig order is :"<<endl;
         for(int i=0;i<n;i++)
         {
             cout<<arr[i]<<endl;
@@ -72,24 +72,45 @@ using namespace std;
 
 //Selection Sort impliment using array
 
-    // void SelectionSort()
-    // {
-    //     int arr[20],n;
-    //     void getdata()
-    //     {
-    //         cout<<"Enter the size of an array:";
-    //         cin>>n;
-    //         cout<<"Enter the value of an arrry:";
-    //         for(int i=0;i<n;i++)
-    //         {
-    //             cin>>arr[i];
-    //         }
-    //     }
+    void SelectionSort(int min)
+    {
+        int n;
+        cout<<"Enter the size of an array :";
+        cin>>n;
+        int arr[n];
+        cout<<"Enter the value of Ascending order :"<<endl;
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
 
-    // }
+
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(arr[j]>arr[min])
+                {
+                    min=j;
+                }
+            }
+            if(min!=i)
+            {
+                int temp;
+                temp=arr[min];
+                arr[min]=arr[i];
+                arr[i]=temp;
+            }
+        }
+
+         cout<<"The all element is Discending order :"<<endl;
+        for(int i=0;i<n;i++)
+        {
+            cout<<arr[i]<<endl;
+        }
+    }
 
     
-
     int main()
     {
          int choise;
@@ -107,8 +128,8 @@ using namespace std;
                 break;
                 case 2: InsertionSort();
                 break;
-                //case 3:getdata();
-                //break;
+                case 3:SelectionSort(0);
+                break;
 
             }
         }
